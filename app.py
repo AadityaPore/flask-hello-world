1125fea8216406c1c5a86064ae404b55ddce9d69
Aaditya Pore <aadityapore@gmail.com>
2024-08-01 04:59:12 +0000

    Inital Updates

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Aaditya Pore in 3308!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://aadis_flask_tut_database_user:YLPHFdNb8zFlr06eH31gczKzUQH5dOBG@dpg-cqlh9cjv2p9s73ardl5g-a/aadis_flask_tut_database")
+    conn.close()
+    return "Database Succesfully Connected!"
